6dbbd4cde782f10802c67aca1bd124a9fa9ddf5b
Nikolay Petukhov <kol.ride.pro.00@gmail.com>
2021-10-29 02:32:52 +0500

    Delete BotTelegram_Currency_Converter directory

--- BotTelegram_Currency_Converter/config.py
+++
@@ -1,5 +0,0 @@
-TOKEN = '2082075078:AAFODCUO4bvmo1FQ2_Xr6peFo7-fomZ6kgc'
-value_key = {'доллар': 'USD',
-             'евро': 'EUR',
-             'рубль': 'RUB',
-             'канадский_доллар': 'CAD'}
